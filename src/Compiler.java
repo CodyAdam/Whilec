82e5b4335a59e25139e29ed09689f4d14a85af89
Thomega35 <thomega35@gmail.com>
2022-11-16 08:39:44 +0100

    functions set TODO for compile in python

--- src/Compiler.java
+++ src/Compiler.java
@@ -14,4 +14,76 @@ public class Compiler {
   public String toString(){
     return getCompiledCode();
   }
+
+  private String rootNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String mainNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String functionNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String expressionsNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String expressionNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String varsNode(CommonTree node) {
+    String t = node.getChild(0).getText();
+
+    return t;
+  } 
+
+  private String varNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String ifNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String forNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String whileNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String assignNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String foreachNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String nopNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
+  private String printNode(CommonTree node) {
+    //TODO
+    return "";
+  }
+
 }
