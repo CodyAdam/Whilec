3f1376e39aa7ed75aab26d336daff3f355e34e63
CodyAdam <cody.ad4@gmail.com>
2022-11-16 08:19:39 +0100

    feat: compiler methods

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,5 +1,17 @@
+import org.antlr.runtime.tree.CommonTree;
+
 public class Compiler {
-  public Compiler() {
-    
+  CommonTree tree;
+  public Compiler(CommonTree root) {
+    tree = root;
+  }
+
+  public String getCompiledCode() {
+    // TODO
+    return "";
+  }
+
+  public String toString(){
+    return getCompiledCode();
   }
 }
