92d559dee36cdbfe6095ed6f2af9214ced8b5f4e
CodyAdam <cody.ad4@gmail.com>
2022-12-07 09:39:03 +0100

    refactor: C3AGenerator

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
-import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.Tree;
+
+import C3A.C3AGenerator;
+import C3A.Instructions;
+
 import org.antlr.runtime.ANTLRFileStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         String filepath;
         if (args.length != 1) {
-            System.err.println("Wrong number of arguments, expected 1, got " + args.length);
-            filepath = "test/nop.while";
+            // System.err.println("Wrong number of arguments, expected 1, got " + args.length);
+            filepath = "test/functions.while";
         } else {
             filepath = args[0];
         }
@@ -20,8 +24,10 @@ public class Main {
 
         var prog = parser.program();
         prog.getStart();
-        CommonTree tree = (CommonTree) prog.getTree();
+        Tree tree = (Tree) prog.getTree();
 
-        System.out.println(tree.getToken());
+        C3AGenerator c3a = new C3AGenerator(tree);
+        Instructions code3adress = c3a.getInstructions();
+        System.out.println(code3adress);
     }
 }
