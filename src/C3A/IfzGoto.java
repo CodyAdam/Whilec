e09c9cb95bc253b02d469859a26a09af1f715153
CodyAdam <cody.ad4@gmail.com>
2023-01-05 17:01:31 +0100

    feat: add label to the if class

--- src/C3A/IfzGoto.java
+++ src/C3A/IfzGoto.java
@@ -1,16 +1,18 @@
 package C3A;
 
 public class IfzGoto extends Instruction {
-  private Label label;
+  private Label elseLabel;
+  private Label endIfLabel;
   private Variable v;
 
-  public IfzGoto(Label label, Variable v) {
-    this.label = label;
+  public IfzGoto(Label elseLabel, Label endIfLabel, Variable v) {
+    this.elseLabel = elseLabel;
+    this.endIfLabel = endIfLabel;
     this.v = v;
   }
 
   @Override
   public String toString() {
-    return "ifz " + v + " goto " + label;
+    return "ifz " + v + " goto " + elseLabel;
   }
 }
