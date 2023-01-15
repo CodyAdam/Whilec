4f3802bcd95c5d28825425c946753c880ced5359
CodyAdam <cody.ad4@gmail.com>
2023-01-15 22:13:34 +0100

    feat: add doc

--- src/C3A/IfzGoto.java
+++ src/C3A/IfzGoto.java
@@ -5,6 +5,11 @@ public class IfzGoto extends Instruction {
   private Label endIfLabel;
   private Variable v;
 
+  /**
+   * If v == 0, do code until elseLabel then jump to endIfLabel
+   * Else, jump to elseLabel and do code until endIfLabel
+   * 
+   */
   public IfzGoto(Label elseLabel, Label endIfLabel, Variable v) {
     this.elseLabel = elseLabel;
     this.endIfLabel = endIfLabel;
