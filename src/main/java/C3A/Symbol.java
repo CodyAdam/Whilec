499583a0e49121bba1f2c9432666106ee4e1dcb9
CodyAdam <cody.ad4@gmail.com>
2023-01-19 00:21:46 +0100

    feat: remove duplicate func name

--- src/main/java/C3A/Symbol.java
+++ src/main/java/C3A/Symbol.java
@@ -1,10 +1,34 @@
 package C3A;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Symbol extends ToAssign {
   private String value;
+  static int count = 0;
+  public static final ArrayList<String> whitelist = new ArrayList<String>(
+      Arrays.asList("main", "bool", "int", "string"));
 
   public Symbol(String value) {
-    this.value = value;
+    if (whitelist.contains(value))
+      this.value = value;
+    else {
+      this.value = value + count;
+      count++;
+    }
+  }
+
+  public Symbol(String value, boolean fixedName) {
+    if (fixedName)
+      this.value = value;
+    else {
+      this.value = value + count;
+      count++;
+    }
+  }
+
+  public String getValue() {
+    return value;
   }
 
   @Override
