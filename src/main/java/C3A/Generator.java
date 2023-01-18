23c260493c6f48c70eaca7b5eb3fcbb4b8faa7e9
CodyAdam <cody.ad4@gmail.com>
2023-01-19 00:19:03 +0100

    feat: add no duplicate symbols +multi func return

--- src/main/java/C3A/Generator.java
+++ src/main/java/C3A/Generator.java
@@ -1,6 +1,7 @@
 package C3A;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
@@ -13,9 +14,11 @@ import AST.ASTParser;
 public class Generator {
   private Instructions instructions;
   private String[] args;
+  private Map<String, Symbol> symbolTable;
 
   public Generator(Tree ast, String[] args) throws Exception {
     this.args = args;
+    this.symbolTable = new HashMap<String, Symbol>();
     instructions = new Instructions();
     instructions = fromRoot(ast);
   }
@@ -41,8 +44,11 @@ public class Generator {
     // Parse Func name
     Tree funcName = ast.getChild(0);
     String funcNameStr = funcName.getText();
+    if (symbolTable.get(funcNameStr) == null)
+      symbolTable.put(funcNameStr, new Symbol(funcNameStr));
+    Symbol funcSymbol = symbolTable.get(funcNameStr);
 
-    i.add(new FuncBegin(funcNameStr));
+    i.add(new FuncBegin(funcSymbol));
 
     // Parse Func definition (input?, commands, output)
     {
@@ -81,7 +87,7 @@ public class Generator {
         String varName = outputNode.getChild(j).getText();
         i.add(new FuncReturn(scopeVars.get(varName)));
       }
-      i.add(new FuncEnd(funcName.getText()));
+      i.add(new FuncEnd(funcSymbol));
     }
 
     return i;
@@ -149,8 +155,8 @@ public class Generator {
     Variable conditionVar = i.getLastAssignedVariable();
 
     // Labels
-    Label elseLabel = new Label("else");
-    Label endIfLabel = new Label("endif");
+    Label elseLabel = new Label("if_else");
+    Label endIfLabel = new Label("if_end");
 
     // Parse if == true commands
     i.add(new IfzGoto(elseLabel, endIfLabel, conditionVar));
@@ -158,8 +164,8 @@ public class Generator {
     i.add(new Goto(endIfLabel));
 
     // Parse else commands
+    i.add(elseLabel);
     if (falseCommandsNode != null) {
-      i.add(elseLabel);
       i.add(fromCommands(falseCommandsNode, scopeVars));
     }
     i.add(endIfLabel);
@@ -184,14 +190,11 @@ public class Generator {
     i.add(new Assign(condition, i.getLastAssignedVariable()));
 
     // Labels
-    Label startWhileLabel = new Label("startwhile");
-    Label endWhileLabel = new Label("endwhile");
-    Label startIfLabel = new Label("startif");
+    Label startWhileLabel = new Label("while_start");
+    Label endWhileLabel = new Label("while_end");
 
     i.add(startWhileLabel);
-    i.add(new IfzGoto(startIfLabel, endWhileLabel, condition));
-    i.add(new Goto(endWhileLabel));
-    i.add(startIfLabel);
+    i.add(new IfzGoto(endWhileLabel, endWhileLabel, condition));
     i.add(fromCommands(commands, scopeVars));
     i.add(new Goto(startWhileLabel));
     i.add(endWhileLabel);
@@ -208,14 +211,11 @@ public class Generator {
     Variable index = i.getLastAssignedVariable();
 
     // For Labels
-    Label startForLabel = new Label("startfor");
-    Label endForLabel = new Label("endfor");
-    Label startIfLabel = new Label("startif");
+    Label startForLabel = new Label("for_start");
+    Label endForLabel = new Label("for_end");
 
     i.add(startForLabel);
-    i.add(new IfzGoto(startIfLabel, endForLabel, index));
-    i.add(new Goto(endForLabel));
-    i.add(startIfLabel);
+    i.add(new IfzGoto(endForLabel, endForLabel, index));
     i.add(fromCommands(commands, scopeVars));
     i.add(new Assign(index, new Unop(UnopEnum.TAIL, index))); // decrement index
     i.add(new Goto(startForLabel));
@@ -242,9 +242,9 @@ public class Generator {
     i.add(new Assign(index, i.getLastAssignedVariable()));
 
     // Labels
-    Label startForLabel = new Label("startfor");
-    Label endForLabel = new Label("endfor");
-    Label startIfLabel = new Label("startif");
+    Label startForLabel = new Label("for_start");
+    Label endForLabel = new Label("for_end");
+    Label startIfLabel = new Label("if_start");
 
     i.add(startForLabel);
     i.add(new IfzGoto(startIfLabel, endForLabel, index));
@@ -278,7 +278,20 @@ public class Generator {
     Tree firstNode = right.getChild(0).getChild(0);
     if (AstNode.valueOf(firstNode.getText()) == AstNode.FUNCTIONCALL) {
       Variable functionCall = new Variable("FUNCCALL");
-      i.add(fromExpr(firstNode, scopeVars));
+      {
+        Tree funcNode = firstNode.getChild(0);
+        String funcName = funcNode.getText();
+        if (symbolTable.get(funcName) == null)
+          symbolTable.put(funcName, new Symbol(funcName));
+        Symbol funcSymbol = symbolTable.get(funcName);
+        int funcChildCount = funcNode.getChildCount();
+        for (int j = 0; j < funcChildCount; j++) {
+          Tree exprNode = funcNode.getChild(j); // EXPRESSION node
+          i.add(fromExpr(exprNode, scopeVars));
+          i.add(new Push(i.getLastAssignedVariable()));
+        }
+        i.add(new Assign(functionCall, new FuncCall(funcSymbol, funcChildCount)));
+      }
       i.add(new Assign(functionCall, i.getLastAssignedVariable()));
       for (int j = 0; j < leftCount; j++) {
         i.add(new Assign(names[j], new TabValue(functionCall, j)));
@@ -372,19 +385,7 @@ public class Generator {
         i.add(new AssignTab(expr, childCount, new Nil()));
         i.add(new Assign(expr, expr));
         break;
-      case FUNCTIONCALL:
-        expr = new Variable("FUNCTIONCALL");
-        Tree funcNode = node.getChild(0);
-        String funcName = funcNode.getText();
-        int funcChildCount = funcNode.getChildCount();
 
-        for (int j = 0; j < funcChildCount; j++) {
-          Tree exprNode = funcNode.getChild(j); // EXPRESSION node
-          i.add(fromExpr(exprNode, scopeVars));
-          i.add(new Push(i.getLastAssignedVariable()));
-        }
-        i.add(new Assign(expr, new FuncCall(funcName, funcChildCount)));
-        break;
       case NIL:
         expr = new Variable("NIL");
         i.add(new Assign(expr, new Nil()));
@@ -398,10 +399,30 @@ public class Generator {
         else
           i.add(new Assign(expr, var));
         break;
+      case FUNCTIONCALL:
+        expr = new Variable("FUNCTIONCALL");
+        Tree funcNode = node.getChild(0);
+        String funcName = funcNode.getText();
+        if (symbolTable.get(funcName) == null)
+          symbolTable.put(funcName, new Symbol(funcName));
+        Symbol funcSymbol = symbolTable.get(funcName);
+
+        int funcChildCount = funcNode.getChildCount();
+
+        for (int j = 0; j < funcChildCount; j++) {
+          Tree exprNode = funcNode.getChild(j); // EXPRESSION node
+          i.add(fromExpr(exprNode, scopeVars));
+          i.add(new Push(i.getLastAssignedVariable()));
+        }
+        Variable temp = new Variable("FUNCCALL_TEMP");
+        i.add(new Assign(temp, new FuncCall(funcSymbol, funcChildCount)));
+        i.add(new Assign(expr, new TabValue(temp, 0)));
+        break;
       case SYMBOL:
         expr = new Variable("SYMBOL");
         String symbolValue = node.getChild(0).getText();
-        i.add(new Assign(expr, new Symbol(symbolValue)));
+        i.add(new Assign(expr, new Symbol(symbolValue, true)));
+        break;
       default:
         throw new Exception(node.getText() + " is not valid child of EXPRESSION");
     }
