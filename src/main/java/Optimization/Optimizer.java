9c107d368b0f6fb577894c49eed37f8c0e9a03c6
yami2200 <theyami22@gmail.com>
2023-01-20 10:21:56 +0100

    add Optimizer class.

---
+++ src/main/java/Optimization/Optimizer.java
@@ -0,0 +1,10 @@
+package Optimization;
+
+public abstract class Optimizer {
+
+    /**
+     * Optimizes the given 3 address code
+     * @param code the 3 address code to optimize
+     */
+    public abstract void optimize(String code);
+}
