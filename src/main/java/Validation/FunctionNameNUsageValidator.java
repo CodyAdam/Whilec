987a4c0fac86fb86955b3a8c77bf26e9cb6268ee
yami2200 <TheYami22@gmail.com>
2023-01-22 17:36:15 +0100

    Edit a function message error.

--- src/main/java/Validation/FunctionNameNUsageValidator.java
+++ src/main/java/Validation/FunctionNameNUsageValidator.java
@@ -37,7 +37,7 @@ public class FunctionNameNUsageValidator extends DeepValidator {
                     this.printError("No function called " + node.getChild(0).getText() + ".", node.getChild(0));
                 }
                 if(this.functions.get(node.getChild(0).getText()).inputOrder.size() != node.getChild(0).getChildCount()) {
-                    this.printError("Bad number of input arguments.", node.getChild(0));
+                    this.printError("Wrong number of input arguments.", node.getChild(0));
                     System.out.println(this.functions.get(node.getChild(0).getText()).inputOrder.size());
                     System.out.println(node.getChildCount());
                     System.out.println(node);
